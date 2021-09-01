a5ccf445487f5e36230302fad1572b70d04f2371
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-09-01 18:52:50 +0530

    LGMVIP- 1st task

--- LGM-VIP_Task-1/CovidStats/app/src/main/java/com/example/covidstats/MainActivity.java
+++ LGM-VIP_Task-1/CovidStats/app/src/main/java/com/example/covidstats/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
